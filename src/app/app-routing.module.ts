e85553ac882547893f3b805736c2764b74e26dd7
Patrik Keinonen <pate@lygon.net>
2018-10-29 13:22:41 +0100

    dynamic page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: '**',
+    component: DynamicPageComponent,
+    pathMatch: 'full',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { TextContentComponent } from './components/text-content/text-content.component';
+import { DynamicPageItemHostDirective } from './pages/dynamic-page/dynamic-page-item-host.directive';
+import { HomeComponent } from './pages/home/home.component';
+import { DynamicComponentLoaderComponent } from './pages/dynamic-page/dynamic-page-item-loader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DynamicPageComponent,
+    BannerComponent,
+    TextContentComponent,
+    DynamicPageItemHostDirective,
+    HomeComponent,
+    DynamicComponentLoaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+  ],
+  entryComponents: [
+    BannerComponent,
+    TextContentComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
